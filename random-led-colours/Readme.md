5e576a1978bd08aa08fa298ffe077e22a96e3965
Manuel <mmartinortiz@gmail.com>
2020-04-22 21:08:05 +0200

    first version of random lights

---
+++ random-led-colours/Readme.md
@@ -0,0 +1,3 @@
+# Random led colours
+
+Change the colour of the leds to a random colour, to choose from red, green or blue.
---
+++ random-led-colours/code.py
@@ -0,0 +1,17 @@
+import time
+from random import choice
+from adafruit_circuitplayground import cp
+
+cp.pixels.brightness = 0.1
+
+def get_color():
+    # Returns a random colour: red, blue or green
+    colours = [(255, 0, 0), (0, 0, 255), (0, 255, 0)]
+    return choice(colours)
+
+while True:
+    for i in range(len(cp.pixels)):
+        # Set a random colour to each led
+        cp.pixels[i] = get_color()
+
+    time.sleep(0.1)
